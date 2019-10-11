4880fdf0c17a7ae2bf80ff811068c90e17093c8e
Jake <panamajake86@gmail.com>
2019-10-10 21:42:31 -0600

    I have finished MVP and 3 of the 4 stretch goals.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import UserForm from "./components/UserForm";
+import EnhancedUserForm from "./components/UserForm";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <EnhancedUserForm />
-      </header>
     </div>
   );
 }
